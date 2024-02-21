73662d7f64155acdd96286f6f12d26a21a9b31b0
victor.mrosa <victor.mrosa@autoglass.com.br>
2024-02-21 08:36:17 -0300

    Atualiza Schemas dos modelos de dados

--- rinha_backend_q1_python/schemas.py
+++ rinha_backend_q1_python/schemas.py
@@ -1,21 +1,30 @@
 from pydantic import BaseModel
+from pydantic import Field
 
-class ClienteTransacaoCreate(BaseModel):
+from datetime import datetime
+from enum import Enum
+from typing import Literal
+
+class EnumTipoTransacao(str, Enum):
+    credito = 'c'
+    debito = 'd'
+
+class RequestTransacao(BaseModel):
+    valor: int = Field(gt=0)
+    tipo: EnumTipoTransacao = Field(description='C - Credito / D - Debito')
+    descricao: str = Field(description='Motivo da transação', min_length=1, max_length=10)
+
+class Transacao(BaseModel):
     valor: int
-    tipo: str
+    tipo: Literal['c', 'd']
     descricao: str
+    realizada_em: datetime = Field(default_factory=datetime.utcnow)
 
-class ClienteTransacoesRead(BaseModel):
-    valor: int
-    tipo: str
-    descricao: str
-    realizada_em: str
-
-class ClienteTransacoesList(BaseModel):
-    ultimas_transacoes: list[ClienteTransacoesRead]
-
-class ClientesRead(BaseModel):
-    id: int
-    nome: str
+class InfoSaldo(BaseModel):
+    total: int
+    data_extrato: datetime = Field(default_factory=datetime.utcnow)
     limite: int
-    saldo: int
+
+class ResponseTransacoes(BaseModel):
+    saldo: InfoSaldo
+    ultimas_transacoes: list[Transacao]
