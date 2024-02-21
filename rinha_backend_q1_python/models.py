2495a80b0da31ca16492a091ef628e8d1c5c669c
victor.mrosa <victor.mrosa@autoglass.com.br>
2024-02-21 08:38:15 -0300

    Atualiza modelagem dos dados

--- rinha_backend_q1_python/models.py
+++ rinha_backend_q1_python/models.py
@@ -1,53 +1,32 @@
-import sqlalchemy as sa
+from sqlalchemy.sql import func
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Table,
+    MetaData,
+    Integer,
+    String
+)
 
-from datetime import datetime
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+metadata_obj = MetaData()
 
-class Base(DeclarativeBase):
-    pass
+clientes = Table(
+    "clientes", 
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("nome", String(50)),
+    Column("limite", Integer),
+    Column("saldo", Integer)
+)
 
-class Clientes(Base):
-    __tablename__ = "clientes"
-
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(sa.String(256))
-    limite: Mapped[int] = mapped_column(sa.Integer)
-    saldo: Mapped[int] = mapped_column(sa.Integer, default=0)
-
-    def __repr__(self) -> str:
-        return (
-            f"<clientes,"
-            f"id={self.id}", 
-            f"nome={self.nome}"
-            f"limite={self.limite}"
-            f"saldo={self.saldo}>"
-        )
-
-    class Config:
-        orm_mode = True
-
-class ClientesTransacoes(Base):
-    __tablename__ = 'clientes_transacoes'
-
-    id: Mapped[int] = mapped_column(sa.Integer, autoincrement=True, primary_key=True)
-    valor: Mapped[int] = mapped_column(sa.Integer, nullable=False)
-    tipo: Mapped[str] = mapped_column(sa.String(1), nullable=False)
-    descricao: Mapped[str] = mapped_column(sa.String(10), nullable=False)
-    realizada_em: Mapped[str] = mapped_column(sa.DateTime(timezone=True), default=datetime.utcnow())
-
-    id_cliente: Mapped[int] = mapped_column(sa.ForeignKey('clientes.id'))
-
-    def __repr__(self) -> str:
-        return (
-            f"<clientes_transacoes,"
-            f"id={self.id}", 
-            f"valor={self.valor}"
-            f"tipo={self.tipo}"
-            f"descricao={self.descricao}"
-            f"realizada_em={self.realizada_em}>"
-        )
-
-    class Config:
-        orm_mode = True
+clientes_transacoes = Table(
+    "clientes_transacoes",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("cliente_id", Integer, ForeignKey("clientes.id")),
+    Column("valor", Integer),
+    Column("tipo", String(1)),
+    Column("descricao", String(10)),
+    Column("realizada_em", DateTime, default=func.now())
+)
