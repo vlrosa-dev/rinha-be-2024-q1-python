facb19ace37b9389d0d301998a1af4c6a7849f47
victor.mrosa <victor.mrosa@autoglass.com.br>
2024-02-21 08:39:12 -0300

    Adiciona configuracao de banco

---
+++ rinha_backend_q1_python/database.py
@@ -0,0 +1,11 @@
+from databases import Database
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+
+URL_DATABASE = 'postgresql+psycopg2://postgres:1234@localhost:5432/postgres'
+
+metadata_obj = MetaData()
+
+database = Database(URL_DATABASE)
+
+engine_db = create_engine(url=URL_DATABASE, echo=True)
